cc1fcabe1ebc8914b838527ddd4be1131cd60708
James Spears <jamesspearsv@gmail.com>
2024-04-28 19:00:12 -0400

    Fix printing bugs

--- src/components/Customizer/Customizer.jsx
+++ src/components/Customizer/Customizer.jsx
@@ -11,25 +11,29 @@ export function Customizer({ style, setStyle }) {
   }
 
   return (
-    <div id='customizer' className='no-print'>
-      <select id='fontFamily' onChange={handelChange}>
-        <option disabled selected>
-          Font Style
-        </option>
-        <option value='serif'>Serif</option>
-        <option value='sans-serif'>Sans-Serif</option>
-      </select>
-      <select id='color' onChange={handelChange}>
-        <option disabled selected>
-          Color
-        </option>
-        <option value='black'>Black</option>
-        <option value='orange'>Orange</option>
-        <option value='purple'>Purple</option>
-        <option value='darkgreen'>Dark Green</option>
-        <option value='dodgerblue'>Dodger Blue</option>
-      </select>
-      <button onClick={window.print}>Export</button>
-    </div>
+    <>
+      <h2 className='no-print'>Customize</h2>
+      <div id='customizer' className='no-print'>
+        <select id='fontFamily' onChange={handelChange}>
+          <option disabled selected>
+            Font Style
+          </option>
+          <option value='serif'>Serif</option>
+          <option value='sans-serif'>Sans-Serif</option>
+          <option value='Cookie'>Cursive</option>
+        </select>
+        <select id='color' onChange={handelChange}>
+          <option disabled selected>
+            Color
+          </option>
+          <option value='black'>Black</option>
+          <option value='orange'>Orange</option>
+          <option value='purple'>Purple</option>
+          <option value='darkgreen'>Dark Green</option>
+          <option value='dodgerblue'>Dodger Blue</option>
+        </select>
+        <button onClick={window.print}>Export</button>
+      </div>
+    </>
   );
 }
